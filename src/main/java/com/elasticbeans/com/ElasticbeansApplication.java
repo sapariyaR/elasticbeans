8345931bff5047d7dead019f4115437a144db7b8
Ravi Sapariya <ravi.sapariya@indexnine.com>
2019-09-25 23:26:11 +0530

    commeting new Springboot code

---
+++ src/main/java/com/elasticbeans/com/ElasticbeansApplication.java
@@ -0,0 +1,13 @@
+package com.elasticbeans.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticbeansApplication.class, args);
+	}
+
+}
